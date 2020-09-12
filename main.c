a56b70915ad8640ee1dee9e642e1b7b6df8a515e
lucasgustavo280 <70767822+lucasgustavo280@users.noreply.github.com>
2020-09-12 09:15:52 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+
+//cálculo área do quadrado
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  float e_lateral1, e_lateral2,s_area;
+
+  scanf("%f",&e_lateral1);
+  scanf("%f",&e_lateral2);
+
+  s_area= e_lateral1*e_lateral2;
+
+printf("area do quadrado:%f\n",s_area);
+
+return 0;
+
+}
+
